cb4f7b68210c24dbc708f8de5a4fde8df4852137
Andrei <andreiflausino@live.com>
2023-12-22 23:46:51 -0300

    Add project files.

---
+++ teste/PokemonClass.cs
@@ -0,0 +1,260 @@
+﻿
+public class Pokemon
+{
+    private string? _name { get; set; }
+    private string? _type { get; set; }
+    private int _hp { get; set; }
+    private int _attack { get; set; }
+    private int _defense { get; set; }
+
+    private List<string> _moves = new List<string>();
+
+    public string? name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string? type
+    {
+        get { return _type; }
+        set { _type = value; }
+    }
+
+    public int hp
+    {
+        get { return _hp; }
+        set { _hp = value; }
+    }
+
+    public int attack
+    {
+        get { return _attack; }
+        set { _attack = value; }
+    }
+
+    public int defense
+    {
+        get { return _defense; }
+        set { _defense = value; }
+    }
+
+    public List<string> moves
+    {
+        get { return _moves; }
+        set { _moves = value; }
+    }
+
+
+    public Pokemon()
+    {
+        _name = string.Empty;
+        _type = string.Empty;
+        _hp = 0;
+        _attack = 0;
+        _defense = 0;
+        _moves = new List<string>();
+    }
+
+
+    public void aiPokemon(Pokemon pokemon)
+    {
+        Random random = new Random();
+        int? aiPokemon = random.Next(1, 4);
+        switch (aiPokemon)
+        {
+            case 1:
+                CreateCharmander(pokemon);
+                break;
+            case 2:
+                CreateSquirtle(pokemon);
+                break;
+            case 3:
+                CreateBulbasaur(pokemon);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CreateCharmander(Pokemon pokemon)
+    {
+        pokemon._name = "Charmander";
+        pokemon._type = "Fire";
+        pokemon._hp = 190;
+        pokemon._attack = 52;
+        pokemon._defense = 36;
+        pokemon._moves = new List<string> { "Ember", "Scratch", "Growl", "Leer" };
+    }
+
+    public void CreateSquirtle(Pokemon pokemon)
+    {
+        pokemon._name = "Squirtle";
+        pokemon._type = "Water";
+        pokemon._hp = 240;
+        pokemon._attack = 48;
+        pokemon._defense = 35;
+        pokemon._moves = new List<string> { "Water Gun", "Tackle", "Tail Whip", "Growl" };
+    }
+
+    public void CreateBulbasaur(Pokemon pokemon)
+    {
+        pokemon._name = "Bulbasaur";
+        pokemon._type = "Grass";
+        pokemon._hp = 250;
+        pokemon._attack = 49;
+        pokemon._defense = 29;
+        pokemon._moves = new List<string> { "Vine Whip", "Tackle", "Growl", "Leech Seed" };
+    }
+
+    public static void Attack(Pokemon player, Pokemon enemy, string move)
+    {
+        Console.WriteLine($"{player._name} used {move}!");
+        int damage = 0;
+
+        if (player._type == "Fire")
+        {
+            if (enemy._type == "Grass")
+            {
+                Console.WriteLine("It's super effective!");
+                damage = (player._attack * 2 - enemy._defense);
+            }
+            else if (enemy._type == "Water")
+            {
+                Console.WriteLine("It's not very effective...");
+                damage = (player._attack / 2 - enemy._defense + 10);
+            }
+            else
+            {
+                damage = (player._attack - enemy._defense);
+            }
+        }
+        else if (player._type == "Water")
+        {
+            if (enemy._type == "Fire")
+            {
+                Console.WriteLine("It's super effective!");
+                damage = (player._attack * 2 - enemy._defense);
+            }
+            else if (enemy._type == "Grass")
+            {
+                Console.WriteLine("It's not very effective...");
+                damage = (player._attack / 2 - enemy._defense + 10);
+            }
+            else
+            {
+                damage = (player._attack - enemy._defense);
+            }
+        }
+        else if (player._type == "Grass")
+        {
+            if (enemy._type == "Water")
+            {
+                Console.WriteLine("It's super effective!");
+                damage = (player._attack * 2 - enemy._defense);
+            }
+            else if (enemy._type == "Fire")
+            {
+                Console.WriteLine("It's not very effective...");
+                damage = (player._attack - enemy._defense + 10);
+            }
+            else
+            {
+                damage = (player._attack - enemy._defense);
+            }
+        }
+
+        damage = Math.Max(0, damage);
+
+        enemy._hp -= damage;
+    }
+    public static void EnemyAttack(Pokemon player, Pokemon enemy)
+    {
+        Random random = new Random();
+        int? aiMove = random.Next(1, 5);
+
+        if (enemy._name == "Charmander")
+        {
+            switch (aiMove)
+            {
+                case 1:
+                    Attack(enemy, player, "Ember");
+                    break;
+                case 2:
+                    Attack(enemy, player, "Scratch");
+                    break;
+                case 3:
+                    Attack(enemy, player, "Growl");
+                    break;
+                case 4:
+                    Attack(enemy, player, "Leer");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (enemy._name == "Squirtle")
+        {
+            switch (aiMove)
+            {
+                case 1:
+                    Attack(enemy, player, "Water Gun");
+                    break;
+                case 2:
+                    Attack(enemy, player, "Tackle");
+                    break;
+                case 3:
+                    Attack(enemy, player, "Tail Whip");
+                    break;
+                case 4:
+                    Attack(enemy, player, "Bubble");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (enemy._name == "Bulbasaur")
+        {
+            switch (aiMove)
+            {
+                case 1:
+                    Attack(enemy, player, "Vine Whip");
+                    break;
+                case 2:
+                    Attack(enemy, player, "Tackle");
+                    break;
+                case 3:
+                    Attack(enemy, player, "Growl");
+                    break;
+                case 4:
+                    Attack(enemy, player, "Leech Seed");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void PlayerAttack(Pokemon player, Pokemon enemy)
+    {
+        Console.WriteLine("Choose your move:");
+        foreach (var _move in player._moves)
+        {
+            Console.WriteLine(_move);
+        }
+        Console.WriteLine();
+        string? playerMove = Console.ReadLine();
+
+        while (player._moves.Contains(playerMove) == false)
+        {
+            Console.WriteLine("This move doesn't exist");
+            Console.WriteLine("Try again using one of these moves:");
+            foreach (var move1 in player._moves)
+            {
+                Console.WriteLine(move1);
+            }
+            playerMove = Console.ReadLine();
+        }
+        Attack(player, enemy, playerMove);
+    }
+}
---
+++ teste/Program.cs
@@ -0,0 +1,103 @@
+﻿Console.WriteLine("Welcome to pokémon, c# edition");
+Console.WriteLine("What's your name?");
+string? name = Console.ReadLine();
+
+string? play = "Y";
+
+while (play == "Y" || play == "")
+{
+
+    Console.WriteLine($"Great! Nice to meet you {name}!\n");
+    Console.WriteLine($"Choose a pokémon {name}!");
+
+    Console.WriteLine("1 - Charmander, 2 - Squirtle, 3 - Bulbasaur");
+
+    int? userPokemonNum = Convert.ToInt32(Console.ReadLine());
+    Pokemon userPokemon = new();
+
+    while (userPokemonNum < 1 || userPokemonNum > 3)
+    {
+        Console.WriteLine("This pokémon doesn't exist");
+        Console.WriteLine("Try again");
+        userPokemonNum = Convert.ToInt32(Console.ReadLine());
+    }
+    if (userPokemonNum == 1)
+    {
+        userPokemon.CreateCharmander(userPokemon);
+        Console.WriteLine($"Congratulations! {userPokemon.name} is now your pokémon!");
+    }
+    else if (userPokemonNum == 2)
+    {
+        userPokemon.CreateSquirtle(userPokemon);
+        Console.WriteLine($"Congratulations! {userPokemon.name} is now your pokémon!");
+    }
+    else if (userPokemonNum == 3)
+    {
+        userPokemon.CreateBulbasaur(userPokemon);
+        Console.WriteLine($"Congratulations! {userPokemon.name} is now your pokémon!");
+    }
+    else
+    {
+        Console.WriteLine("This pokémon doesn't exist");
+    }
+    Console.WriteLine();
+
+
+    Console.WriteLine("\nIt's time for a pokémon battle!");
+    Pokemon aiPokemon = new Pokemon();
+    aiPokemon.aiPokemon(aiPokemon);
+    Console.WriteLine($"Your opponent threw out a {aiPokemon.name}");
+
+    Random random = new Random();
+
+    int randomNum = random.Next(1, 3);
+
+
+    if (randomNum == 1)
+    {
+        Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+        while ((aiPokemon.hp > 0 || userPokemon.hp > 0))
+        {
+            if (aiPokemon.hp <= 0)
+            {
+                Console.WriteLine("Congratulations! You won!");
+                break;
+            }
+            else if (userPokemon.hp <= 0)
+            {
+                Console.WriteLine("Your opponent won this time...");
+                break;
+            }
+            Pokemon.PlayerAttack(userPokemon, aiPokemon);
+            Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+            Pokemon.EnemyAttack(userPokemon, aiPokemon);
+            Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+        }
+    }
+
+    else if (randomNum == 2)
+    {
+        Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+        while ((aiPokemon.hp > 0 || userPokemon.hp > 0))
+        {
+            if (aiPokemon.hp <= 0)
+            {
+                Console.WriteLine("Congratulations! You won!");
+                break;
+            }
+            else if (userPokemon.hp <= 0)
+            {
+                Console.WriteLine("Your opponent won this time...");
+                break;
+            }
+            Pokemon.EnemyAttack(userPokemon, aiPokemon);
+            Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+            Pokemon.PlayerAttack(userPokemon, aiPokemon);
+            Console.WriteLine($"\nYour pokémon's hp: {userPokemon.hp} vs Your opponent's pokémon's hp: {aiPokemon.hp}\n");
+        }
+    }
+
+
+    Console.WriteLine("Play again?");
+    play = Console.ReadLine();
+}
